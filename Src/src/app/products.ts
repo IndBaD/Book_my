21b58fdf5bdaa85b9a53e99865f30f14afc2eb30
IndBaD <76472752+IndBaD@users.noreply.github.com>
2022-10-19 02:34:10 +0300

    Add files via upload

---
+++ Src/src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    name: 'Идиот',
+    author: 'Достоевский Ф.М.',
+    genre: 'Трагедия',
+    image: 'https://ir.ozone.ru/s3/multimedia-7/6008818891.jpg'
+  },
+  {
+    name: 'Метро 2033',
+    author: 'Дмитрий Алексеевич Глуховский',
+    genre: 'Роман',
+    image: 'https://cv9.litres.ru/pub/c/elektronnaya-kniga/cover_max1500/128391-dmitriy-gluhovskiy-metro-2033.jpg'
+  },
+  {
+    name: 'Программист за 1 день',
+    author: 'Я',
+    genre: 'Комедия',
+    image: 'https://cs12.pikabu.ru/post_img/big/2020/03/18/8/158453465616334067.jpg'
+  }
+];
